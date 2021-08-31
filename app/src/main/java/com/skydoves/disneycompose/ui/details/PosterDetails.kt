00f2bb0cf8a6ffc95ad4f10cc49d9dc4e1c17da5
skydoves <skydoves@naver.com>
2021-09-01 00:19:10 +0900

    Implement GIF for PosterDetails

--- app/src/main/java/com/skydoves/disneycompose/ui/details/PosterDetails.kt
+++ app/src/main/java/com/skydoves/disneycompose/ui/details/PosterDetails.kt
@@ -16,6 +16,7 @@
 
 package com.skydoves.disneycompose.ui.details
 
+import android.os.Build
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -27,9 +28,11 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -43,14 +46,24 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.palette.graphics.Palette
+import coil.ImageLoader
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
 import com.skydoves.disneycompose.model.Poster
 import com.skydoves.disneycompose.ui.custom.ImageBalloonAnchor
+import com.skydoves.disneycompose.ui.theme.background800
+import com.skydoves.disneycompose.ui.theme.shimmerHighLight
 import com.skydoves.disneycompose.utils.NetworkImage
+import com.skydoves.landscapist.ShimmerParams
+import com.skydoves.landscapist.coil.CoilImage
 import com.skydoves.landscapist.palette.BitmapPalette
 
 @Composable
@@ -59,76 +72,130 @@ fun PosterDetails(
   pressOnBack: () -> Unit
 ) {
   val details: Poster? by viewModel.posterDetails.observeAsState()
-  var palette by remember { mutableStateOf<Palette?>(null) }
   details?.let { poster ->
-    Column(
-      modifier = Modifier
-        .verticalScroll(rememberScrollState())
-        .background(MaterialTheme.colors.background)
-        .fillMaxHeight()
-    ) {
-      ConstraintLayout {
-        val (arrow, image, paletteRow, title, content) = createRefs()
-        NetworkImage(
-          url = poster.poster,
-          modifier = Modifier
-            .constrainAs(image) {
-              top.linkTo(parent.top)
-            }
-            .fillMaxWidth()
-            .aspectRatio(0.85f),
-          circularRevealedEnabled = true,
-          bitmapPalette = BitmapPalette {
-            palette = it
+    PosterDetailsBody(poster, pressOnBack)
+  }
+}
+
+@Composable
+fun PosterDetailsBody(
+  poster: Poster,
+  pressOnBack: () -> Unit
+) {
+  Column(
+    modifier = Modifier
+      .verticalScroll(rememberScrollState())
+      .background(MaterialTheme.colors.background)
+      .fillMaxHeight()
+  ) {
+    var palette by remember { mutableStateOf<Palette?>(null) }
+
+    ConstraintLayout {
+      val (arrow, image, paletteRow, title, content, gifTitle, gif) = createRefs()
+
+      NetworkImage(
+        url = poster.poster,
+        modifier = Modifier
+          .constrainAs(image) {
+            top.linkTo(parent.top)
           }
-        )
-        ColorPalettes(
-          palette = palette,
-          modifier = Modifier
-            .constrainAs(paletteRow) {
-              top.linkTo(image.bottom)
-            }
-        )
-        Text(
-          text = poster.name,
-          style = MaterialTheme.typography.h1,
-          overflow = TextOverflow.Ellipsis,
-          maxLines = 1,
-          modifier = Modifier
-            .constrainAs(title) {
-              top.linkTo(paletteRow.bottom)
-            }
-            .padding(start = 16.dp, top = 12.dp)
-        )
-        Text(
-          text = poster.description,
-          style = MaterialTheme.typography.body2,
-          modifier = Modifier
-            .constrainAs(content) {
-              top.linkTo(title.bottom)
-            }
-            .padding(16.dp)
-        )
-        ImageBalloonAnchor(
-          reference = image,
-          modifier = Modifier
-            .fillMaxWidth()
-            .aspectRatio(0.85f),
-          content = poster.name,
-          onClick = { balloon, anchor -> balloon.showAlignBottom(anchor) }
-        )
-        Icon(
-          imageVector = Icons.Filled.ArrowBack,
-          tint = Color.White,
-          contentDescription = null,
-          modifier = Modifier
-            .constrainAs(arrow) {
-              top.linkTo(parent.top)
-            }
-            .padding(12.dp)
-            .clickable(onClick = { pressOnBack() })
-        )
-      }
+          .fillMaxWidth()
+          .aspectRatio(0.85f),
+        circularRevealedEnabled = true,
+        bitmapPalette = BitmapPalette {
+          palette = it
+        }
+      )
+
+      ColorPalettes(
+        palette = palette,
+        modifier = Modifier
+          .constrainAs(paletteRow) {
+            top.linkTo(image.bottom)
+          }
+      )
+
+      Text(
+        text = poster.name,
+        style = MaterialTheme.typography.h1,
+        overflow = TextOverflow.Ellipsis,
+        maxLines = 1,
+        modifier = Modifier
+          .constrainAs(title) {
+            top.linkTo(paletteRow.bottom)
+          }
+          .padding(start = 16.dp, top = 12.dp)
+      )
+
+      Text(
+        text = poster.description,
+        style = MaterialTheme.typography.body2,
+        modifier = Modifier
+          .constrainAs(content) {
+            top.linkTo(title.bottom)
+          }
+          .padding(16.dp)
+      )
+
+      Text(
+        text = "Gif",
+        style = MaterialTheme.typography.h2,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+          .padding(8.dp)
+          .constrainAs(gifTitle) {
+            top.linkTo(content.bottom)
+          }
+      )
+
+      val context = LocalContext.current
+      val imageLoader = ImageLoader.Builder(context)
+        .componentRegistry {
+          if (Build.VERSION.SDK_INT >= 28) {
+            add(ImageDecoderDecoder(context))
+          } else {
+            add(GifDecoder())
+          }
+        }
+        .build()
+
+      CoilImage(
+        imageModel = poster.gif,
+        imageLoader = imageLoader,
+        shimmerParams = ShimmerParams(
+          baseColor = background800,
+          highlightColor = shimmerHighLight
+        ),
+        modifier = Modifier
+          .fillMaxWidth()
+          .padding(8.dp)
+          .height(500.dp)
+          .clip(RoundedCornerShape(8.dp))
+          .constrainAs(gif) {
+            top.linkTo(gifTitle.bottom)
+          }
+      )
+
+      ImageBalloonAnchor(
+        reference = image,
+        modifier = Modifier
+          .fillMaxWidth()
+          .aspectRatio(0.85f),
+        content = poster.name,
+        onClick = { balloon, anchor -> balloon.showAlignBottom(anchor) }
+      )
+
+      Icon(
+        imageVector = Icons.Filled.ArrowBack,
+        tint = Color.White,
+        contentDescription = null,
+        modifier = Modifier
+          .constrainAs(arrow) {
+            top.linkTo(parent.top)
+          }
+          .padding(12.dp)
+          .clickable(onClick = { pressOnBack() })
+      )
     }
   }
 }
