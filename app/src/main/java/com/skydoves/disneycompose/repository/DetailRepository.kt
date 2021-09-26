e9e9caad731ccb43abb13c73c993410cc218881a
skydoves <skydoves@naver.com>
2021-09-26 23:08:49 +0900

    Refactor getPoster as suspend function on PosterDao

--- app/src/main/java/com/skydoves/disneycompose/repository/DetailRepository.kt
+++ app/src/main/java/com/skydoves/disneycompose/repository/DetailRepository.kt
@@ -16,12 +16,20 @@
 
 package com.skydoves.disneycompose.repository
 
+import androidx.annotation.WorkerThread
 import com.skydoves.disneycompose.persistence.PosterDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class DetailRepository @Inject constructor(
   private val posterDao: PosterDao
 ) : Repository {
 
-  fun getPosterById(id: Long) = posterDao.getPoster(id)
+  @WorkerThread
+  fun getPosterById(id: Long) = flow {
+    val poster = posterDao.getPoster(id)
+    emit(poster)
+  }.flowOn(Dispatchers.IO)
 }
--- app/src/main/java/com/skydoves/disneycompose/ui/details/PosterDetails.kt
+++ app/src/main/java/com/skydoves/disneycompose/ui/details/PosterDetails.kt
@@ -41,8 +41,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -74,10 +74,10 @@ fun PosterDetails(
   pressOnBack: () -> Unit
 ) {
   LaunchedEffect(key1 = posterId) {
-    viewModel.getPoster(posterId)
+    viewModel.loadPosterById(posterId)
   }
 
-  val details: Poster? by viewModel.posterDetails.observeAsState()
+  val details: Poster? by viewModel.posterDetailsFlow.collectAsState(initial = null)
   details?.let { poster ->
     PosterDetailsBody(poster, pressOnBack)
   }
